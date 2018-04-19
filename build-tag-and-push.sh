5e31137a04b6faca701287e46b8281b84280b3ad
Justin Ramos <justin@gigster.com>
2018-04-19 10:34:44 -0700

    set region

--- build-tag-and-push.sh
+++ build-tag-and-push.sh
@@ -2,7 +2,7 @@
 
 ECR=804879515677.dkr.ecr.us-east-1.amazonaws.com
 
-eval $(aws ecr get-login --no-include-email)
+eval $(aws ecr get-login --no-include-email --region us-east-1)
 
 docker build -t glitterbot:latest .
 docker tag glitterbot:latest $ECR/glitterbot:latest
