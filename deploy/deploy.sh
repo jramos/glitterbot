49b50f1e9b2b94138c9209ecb379a79b38a4c7b6
Justin Ramos <justin@gigster.com>
2019-01-10 08:57:53 -0800

    shrink docker context, update deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-ECR=804879515677.dkr.ecr.us-east-1.amazonaws.com
+KUBECTL=${KUBECTL:-"$HOME/bin/kubectl-v1.9.11"}
+ECR="804879515677.dkr.ecr.us-east-1.amazonaws.com"
+TAG=${TAG:-"latest"}
 
 eval $(aws ecr get-login --no-include-email --region us-east-1)
 
-docker build -t glitterbot:latest .
-docker tag glitterbot:latest $ECR/glitterbot:latest
-docker push $ECR/glitterbot:latest
+docker build -t glitterbot:${TAG} .
+docker tag glitterbot:${TAG} $ECR/glitterbot:${TAG}
+docker push $ECR/glitterbot:${TAG}
 
 for DIR in en ja nl; do
   aws s3 sync public/$DIR/ s3://glitterbot/$DIR/ --acl public-read --delete
 done
 
-kubectl delete -f deploy/deployment.yml
-kubectl create -f deploy/deployment.yml
+$KUBECTL delete -f deploy/deployment.yml
+$KUBECTL create -f deploy/deployment.yml
